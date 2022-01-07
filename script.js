85a857e75fae5d6a3d49c9fb05ec23a94e35566d
RitaEstevinhaSilva <ritaestevinhasilva@gmail.com>
2022-01-07 19:02:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
 
 tl.to('.intro-text', {y: '0%', duration: 1, stagger: 0.2}); //stagger: 0.25
-// tl.to ('.slider', {y: "-100%", duration:3, delay: 0.5});
 tl.to('.intro', {y: "-100%", duration: 0.8, delay: 0.5} ); //, "-=1"
 tl.fromTo("#section--top", {opacity:0}, {opacity: 1, duration: 1});
 tl.fromTo(".bottom", {opacity:0}, {opacity: 1, duration: 1} , "-=1");
